b023e072280ef13338dd343fdbc543e56e69f6c4
Presly KOMBA MALONGO <preslykomba.m@gmail.com>
2022-12-24 10:41:06 +0100

    Get data + Ajout de mutiples "function"

--- front/js/script.js
+++ front/js/script.js
@@ -2,16 +2,27 @@ fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
     .then((data) => addProducts(data))
 
-        function addProducts(donnees) {
-            const imageUrl = donnees[0].imageUrl
-            
-            const anchor = document.createElement("a")
-            anchor.href = imageUrl
-            anchor.text = "Un super canapé"
-            const items = document.querySelector("#items")
-            if (items != null) {
-                items.appendChild(anchor)
-            }
-        }
+function addProducts(donnees) {
+    const imageUrl = donnees[0].imageUrl
+    const anchor = makeAnchor(imageUrl)
+    appendChildren(anchor)
+}
+
+function makeAnchor(url) {
+    const anchor = document.createElement("a")
+    anchor.href = url
+    return anchor
+}
+
+function appendChildren(anchor) {
+    const items = document.querySelector("#items")
+    if (items != null) {
+        items.appendChild(anchor)
+    }
+}
+
+
+
+
 
 
